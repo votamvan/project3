2a3b273902106e8272aed3e781ae063bf5bda76c
Vo Tam Van <vtvan2k1@gmail.com>
2020-05-13 11:43:40 -0500

    add new yolo shoes model

--- python-src/app/models/yolov3_keras.py
+++ python-src/app/models/yolov3_keras.py
@@ -6,15 +6,14 @@ import imutils
 from config import UPLOAD_FOLDER
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
-yolo_weights = "shoes_all_yolov3_tiny_5000.weights"
+yolo_weights = "shoes_all_yolov3_3000.weights"
 yolo_data = "shoes_all.data"
-yolo_config = "shoes_all_yolov3_tiny.cfg"
+yolo_config = "shoes_all_yolov3.cfg"
 
 
 def create_yolov3_detect(filename):
     filename = UPLOAD_FOLDER + filename
     output_fname = "IMAGE_YOLOV3.png"
-
     command = f"cd {dir_path};"
     command = command + "${PWD}/darknet detector test " + f"{yolo_data} {yolo_config} {yolo_weights} '{filename}';"
     command = command + f"mv predictions.jpg {UPLOAD_FOLDER}{output_fname}"
@@ -22,5 +21,10 @@ def create_yolov3_detect(filename):
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output = proc.stdout.read()
     print(output)
-    return output_fname
+    if not output:  # cannot detect
+        return filename, ""
+    arr = output.decode('ascii')[:-1].split("\n")
+    label = ".".join(arr[1:])
+    print(label)
+    return output_fname, label
 
